fd1353ca066e1edafa96c661f992a393e968c31b
Sam Wray <Sam.Wray@twentysixdigital.com>
2021-03-19 14:41:42 +0000

    added react snap

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
+import ynwa from './ynwa.mp3';
 import './App.css';
+const audio = new Audio(ynwa);
+
+const playSound = () => {
+  audio.play();
+};
+const pauseSound = () => {
+  audio.pause();
+};
+
+const template = (
+  <div className='App'>
+    <header className='App-header'>
+      <h1>Welcome intrigued friends!</h1>
+    </header>
+    <main>
+      <img src='/images/lfc.jpeg' alt='LFC' />
+
+      <button type='button' onClick={playSound}>
+        Play
+      </button>
+      <button type='button' onClick={pauseSound}>
+        Pause
+      </button>
+    </main>
+  </div>
+);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return template;
 }
 
 export default App;
